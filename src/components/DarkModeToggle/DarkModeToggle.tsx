21d6c61ba33f3a851cbfc813e33d6c10a1c3994d
Shazzy911 <Shazzydev911@gmail.com>
2023-12-11 00:59:16 +0500

    changes complete

--- src/components/DarkModeToggle/DarkModeToggle.tsx
+++ src/components/DarkModeToggle/DarkModeToggle.tsx
@@ -6,9 +6,6 @@ import { setToggleOpen } from "@/app/redux/toggleSlice";
 export default function DarkModeToggle() {
   const dispatch = useAppDispatch();
   const toggleOpen = useAppSelector((state: any ) => state.toggle.toggleOpen);
- localStorage.setItem("togggleOpen", toggleOpen);
-// console.log(toggleOpen);
-  
   return (
     // JSX
     <div className={styles.container}  onClick={() => dispatch(setToggleOpen(!toggleOpen))} >  
